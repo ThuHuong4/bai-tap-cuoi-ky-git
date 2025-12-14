d91d0bc382c8d867bfa99f56cff84e84ddafce1a
ThuHuong4 <huynhlethuhuong@gmail.com>
2025-12-14 14:38:54 +0700

    Update server.js

--- project-1-node-render/server.js
+++ project-1-node-render/server.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Project 1 Ready!'));
+app.get('/', (req, res) => res.send('<h1>Hello Huong - Project 1 Success</h1>'));
 app.listen(3000);
